cff9888cd2e690acf995319e498d6940b698902f
Christopher Day <dayo@cddesigns.org>
2018-01-18 19:49:03 +0000

    Renamed pings table to hosts, also started work on windows host monitoring

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var server = require("./lib/http");
 var socket = require("./lib/socket");
 
 setInterval(() => {
-	let pings = db.pings.find().forEach((host) => {
+	db.hosts.find().forEach((host) => {
 		let pingTime = new Date().valueOf();
 		ping.sys.probe(host.hostname, (isAlive) => {
 			let options = { online: isAlive, lastCheck: new Date().valueOf() };
@@ -13,12 +13,12 @@ setInterval(() => {
 				options.lastOnline = new Date().valueOf();
 				options.pingTime = options.lastOnline - pingTime;
 			}
-			db.pings.update({
+			db.hosts.update({
 				id: host.id
 			}, options);
 		});
 	});
-	socket.emit("pings", db.pings.find().filter((r)=> { 
+	socket.emit("pings", db.hosts.find().filter((r)=> { 
 		delete r._id; return r; 
 	}));
 }, config.pingInterval);
